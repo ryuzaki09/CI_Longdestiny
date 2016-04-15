dabdab2c3372657c22eac49e13742733bb4f61b7
ryuzaki <ryuzaki@sinluong.com>
2016-04-15 15:34:32 +0000

    updated admin menu setup to be harded in view so it is not lost if it is not in the DB.

--- application/views/admin/adminmenu2.php
+++ application/views/admin/adminmenu2.php
@@ -1,26 +1,4 @@
 <div id="accordion" style="width:140px; float:left; text-align:center; font-size:12px; margin-right:20px; padding:10px 0;">
-    <!-- <h4><a href="#">Admin Home</a></h4> -->
-    <!-- <div class="marg7_0"> -->
-    <!--     <a href="<?php //echo base_url(); ?>admin/home">Home</a> -->
-    <!-- </div> -->
-    <!--  -->
-    <!-- <h4><a href="#">Front Page</a></h4> -->
-    <!-- <div class="marg7_0"> -->
-    <!--     <a href="<?php //echo base_url(); ?>admin/fpwindows/addnew">Add new window</a><br/> -->
-    <!--     <a href="<?php //echo base_url(); ?>admin/fpwindows/listing">Windows List</a> -->
-    <!-- </div> -->
-    <!--  -->
-    <!-- <h4><a href="#">Photos</a></h4> -->
-    <!-- <div class="marg7_0"> -->
-    <!--     <a href="<?php //echo base_url(); ?>admin/photos/addnew">Add Photo</a><br/> -->
-    <!--     <a href="<?php //echo base_url(); ?>admin/photos/albumlist">List Albums</a> -->
-    <!-- </div> -->
-    <!--  -->
-    <!-- <h4><a href="#">Portfolio</a></h4> -->
-    <!-- <div class="marg7_0"> -->
-    <!--     <a href="/admin/portfolio/addnew">Add Portfolio</a><br/> -->
-    <!--     <a href="/admin/portfolio/listing">List Portfolio</a> -->
-    <!-- </div> -->
 	<?php
 	if(!empty($menu_array)){
 		foreach($menu_array AS $parent):
@@ -39,6 +17,11 @@
 	}
 
 	?>
+    <h4><a href="#">Admin Menu</a></h4>
+    <div class="marg7_0">
+        <a href="/admin/home/menusetup">Menu setup</a><br />
+        <a href="/admin/home/menulist">Menu List</a>
+    </div>
 </div>
 
 
